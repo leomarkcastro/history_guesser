9b32629ffe86d71f524627288204a23504d14f78
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2023-05-27 13:42:20 +0800

    fixed layout of landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-base-100 text-base-content">
+    <main className="relative w-screen h-screen bg-base-100 text-base-content">
       <div className="flex flex-col items-center justify-center w-full h-full gap-2 p-4 text-center">
         <h1 className="text-4xl">History Guesser</h1>
         <p>Guess the year, month and day of an event in history</p>
         <Link href="/play" className="d-btn d-btn-primary">
           Play
         </Link>
-        <div className="mt-24">
-          <p className="font-bold">Credits</p>
-          <p>Dates were scrapped from Wikipedia</p>
-          <p>
-            Developed by{" "}
-            <a href="https://dev.leocastro.com" className="d-link">
-              Leo Castro
-            </a>
-          </p>
-        </div>
+      </div>
+      <div className="absolute left-0 right-0 mt-24 text-center bottom-2">
+        <p className="font-bold">Credits</p>
+        <p>Dates were scrapped from Wikipedia</p>
+        <p>
+          Developed by{" "}
+          <a href="https://dev.leocastro.com" className="d-link">
+            Leo Castro
+          </a>
+        </p>
       </div>
     </main>
   );
